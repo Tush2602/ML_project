7ba1fda8ff4bfacb99218e83844915a519045e39
Tushar Joshi <Tush2602@gmail.com>
2025-05-15 11:29:28 +0530

    EDA and problem statement.

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb =error_detail.exc_info()
+    _,_,exc_tb =error_detail.exc_info()     # sys.exc_info() == returns 3 value exception type , exc value, Traceback object
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(file_name, exc_tb.tb_lineno, str(error))
 
@@ -19,3 +19,10 @@ class CustomException(Exception):
         return self.error_message
     
 
+# if __name__ == "__main__":
+#
+#     try:
+#         a=1/0
+#     except Exception as E:
+#         logging.info("Divide by zero error.")
+#         raise CustomException(E, sys)
