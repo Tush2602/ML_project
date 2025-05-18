fd5c0448969d4010d206e9a7d02a1730e28fabcf
Tushar Joshi <Tush2602@gmail.com>
2025-05-18 17:52:46 +0530

    Deployment configuration for AWS

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="tushar",
+    author_email="tush2602@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
